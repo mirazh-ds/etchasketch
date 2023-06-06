74b28ea29c989865126a7295de87ce0c1fe94fde
mirazh-ds <131595327+mirazh-ds@users.noreply.github.com>
2023-06-06 13:56:18 +0200

    aggiunge tasto erase, colori con prompt

--- script.js
+++ script.js
@@ -6,29 +6,33 @@ var size;
 var sizeW;
 var sizeH;
 
+createGrid();
+
 function sizePrompt () {       
 
-        let sizePrompt = prompt("Select grid height (must be a mutiple of 16, between 16 and 144):")
+        let sizePrompt = prompt("Type grid width (must be a divisor of 960 (16, 20, 24, 30, 32, 40, 48, 60, 64, 80, 96, 120, 160), between 16 and 160:")
         size = parseInt(sizePrompt)*parseInt(sizePrompt);
         sizeH = 960/sizePrompt;
         sizeW = 960/sizePrompt;
 
         if (parseInt(sizePrompt) > 160) {
 
-                alert("Size too high (must be a divider of 960, between 16 and 160)!");
+                alert("Size too high (must be between 16 and 160)!");
                 } else if (parseInt(sizePrompt) < 16) {
-                alert("Size too small (must be a divider of 960, between 16 and 160)!");
+                alert("Size too small (must be between 16 and 160)!");
                 } else if (Number.isInteger(sizeW)) {
                 createGrid();
                 } else if (isNaN(sizePrompt)) {
                         alert("Not a number.")
                 } else {
-                        alert("Not a divider of 960.")
+                        alert("Not a divisor of 960.")
                 }
 
 }
 
 function createGrid() {
+
+        container.replaceChildren();
 
         for (let y = 0; y < size; y++) {
 
@@ -45,15 +49,23 @@ function createGrid() {
 
 function paint() {
 
-        const cells = container.querySelectorAll('#cellH');
+        let color = prompt('Type in your color (ex.: "black", "white", "red", "lightblue"...):')
 
+        const cells = container.querySelectorAll('#cellH');
         cells.forEach(cellH => cellH.addEventListener("mouseover", () => {
 
-                cellH.style.backgroundColor = "black";
+                cellH.style.backgroundColor = `${color}`;
 
         })
         )
 
+}
+
+function erase() {
+        const cells = container.querySelectorAll('#cellH');
+        cells.forEach(cellH => cellH.addEventListener("mouseover", () => {
+                cellH.style.backgroundColor = "white";
+        }))
 }
 
 function reset() {
@@ -62,6 +74,8 @@ function reset() {
         cells.forEach(cellH => cellH.style.backgroundColor = "white")
 
 }
+
+
 
 
 
